db2f8116c44d82a83a8bd647f05c60244eb0cb4c
f7q <kokudou330@gmail.com>
2017-06-17 14:59:39 +0900

    Add Tests

--- src/IdentityDirectory.Scim/Expressions/SortExpressionParser.cs
+++ src/IdentityDirectory.Scim/Expressions/SortExpressionParser.cs
@@ -13,13 +13,8 @@
 
         private static readonly Parser<ScimExpression> Filter;
         private static readonly Parser<ScimExpression> ExpressionInParentheses;
-        private static readonly Parser<string> OrderString;
-        private static readonly Parser<string> QuotedString;
-        private static readonly Parser<ScimExpression> IdentifierName;
         private static readonly Parser<ScimExpression> CaseInsensitiveString;
         private static readonly Parser<ScimExpression> Operand;
-        private static readonly Parser<ScimExpression> AttributeExpression;
-        private static readonly Parser<ScimExpression> Comparison;
         private static readonly Parser<ScimExpression> Presence;
 
         /// <summary>
@@ -27,17 +22,6 @@
         /// </summary>
         static SortExpressionParser()
         {
-            /*
-            // Key値 [a-zA-Z][_a-zA-Z0-9]+
-            IdentifierName = from expr in Parse.Ref(() => Filter)
-                             select expr;
-            QuotedString = from space in Parse.WhiteSpace.Optional()
-                           from lparen in Parse.Char('-').Or(Parse.Char('d')).Optional()
-                            from content in Parse.LetterOrDigit.Or(Parse.Chars("_")).Many().Text()
-                            //from space in Parse.WhiteSpace
-                            //from rparen in Parse.String("desc").Or(Parse.String("asc"))
-                            select content;*/
-            
             CaseInsensitiveString = from space in Parse.WhiteSpace.Optional()
                                     from lparen in Parse.Char('-').Or(Parse.Char('d')).Optional()
                                     from content in Parse.LetterOrDigit.Or(Parse.Chars("_")).Many().Text()
@@ -53,16 +37,9 @@
 
             // compareOp = "-" / "d" valuePath "desc" /
             //        valuePath" / "" / "asc" /
-            /*Comparison = Parse.XChainOperator(
-                Descending
-                .XOr(Ascending)
-                , Operand, ScimExpression.Binary);*/
-
             Presence = Operand.SelectMany(operand => Descending.Or(Ascending), (operand, pr) => ScimExpression.Unary(pr, operand));
-
-            //AttributeExpression = Presence.Or(Comparison);
+            
             // logExp    = FILTER SP (",") SP FILTER
-            //Filter = Parse.XChainOperator(Delimiter, Comparison, ScimExpression.Binary);
             Filter = Parse.XChainOperator(Delimiter, Presence, ScimExpression.Binary);
         }
 
--- test/IdentityDirectory.Scim.Test/SortExpressionParserTests.cs
+++ test/IdentityDirectory.Scim.Test/SortExpressionParserTests.cs
@@ -14,5 +14,41 @@
             Console.WriteLine(rootNode);
             Assert.Equal("Delimiter(Delimiter(Delimiter(Ascending(Item1),Descending(Item2)),Ascending(Item3)),Descending(Item4))", rootNode.ToString());
         }
+
+        [Fact]
+        public void CanParseSimpleFilter2()
+        {
+            var rootNode = SortExpressionParser.ParseExpression("Item1 asc, Item2 desc, Item3 asc");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Delimiter(Delimiter(Ascending(Item1),Descending(Item2)),Ascending(Item3))", rootNode.ToString());
+        }
+
+        [Fact]
+        public void CanParseSimpleFilter3()
+        {
+            var rootNode = SortExpressionParser.ParseExpression("Item1 asc, Item2 desc");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Delimiter(Ascending(Item1),Descending(Item2))", rootNode.ToString());
+        }
+
+        [Fact]
+        public void CanParseSimpleFilter4()
+        {
+            var rootNode = SortExpressionParser.ParseExpression("Item1 asc");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Ascending(Item1)", rootNode.ToString());
+        }
+
+        [Fact]
+        public void CanParseSimpleFilter5()
+        {
+            var rootNode = SortExpressionParser.ParseExpression("Item1 desc");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Descending(Item1)", rootNode.ToString());
+        }
     }
 }
