f22b6bf8173f70819aaf5ecedb9799b77a277902
f7q <kokudou330@gmail.com>
2017-06-17 14:53:58 +0900

    Add OrderBy Exsspression Filter

--- src/IdentityDirectory.Scim/Expressions/SortExpressionParser.cs
+++ src/IdentityDirectory.Scim/Expressions/SortExpressionParser.cs
@@ -6,16 +6,19 @@
 
     public class SortExpressionParser
     {
-        private static readonly Parser<string> Descending = Parse.String("desc").Or(Parse.String("-")).Or(Parse.String("d")).Token().Return("Descending");
+        private static readonly Parser<string> Descending = Parse.String("desc").Token().Return("Descending");
         private static readonly Parser<string> Ascending = Parse.String("asc").Token().Return("Ascending");
-        private static readonly Parser<string> Delimiter = Parse.String(",").Token().Return("Delimiter");
-        
+        private static readonly Parser<string> Delimiter = Parse.Char(',').Token().Return("Delimiter");
+
+
         private static readonly Parser<ScimExpression> Filter;
-        private static readonly Parser<ScimExpression> ExpressionInParentheses = from lparen in Parse.Char('-').Or(Parse.Char('d')).Optional()
-                                                                                 from expr in Parse.Ref(() => Filter)
-                                                                                 from rparen in Parse.String("desc").Or(Parse.String("asc")).Optional()
-                                                                                 select expr;
-        
+        private static readonly Parser<ScimExpression> ExpressionInParentheses;
+        private static readonly Parser<string> OrderString;
+        private static readonly Parser<string> QuotedString;
+        private static readonly Parser<ScimExpression> IdentifierName;
+        private static readonly Parser<ScimExpression> CaseInsensitiveString;
+        private static readonly Parser<ScimExpression> Operand;
+        private static readonly Parser<ScimExpression> AttributeExpression;
         private static readonly Parser<ScimExpression> Comparison;
         private static readonly Parser<ScimExpression> Presence;
 
@@ -24,16 +27,43 @@
         /// </summary>
         static SortExpressionParser()
         {
+            /*
+            // Key値 [a-zA-Z][_a-zA-Z0-9]+
+            IdentifierName = from expr in Parse.Ref(() => Filter)
+                             select expr;
+            QuotedString = from space in Parse.WhiteSpace.Optional()
+                           from lparen in Parse.Char('-').Or(Parse.Char('d')).Optional()
+                            from content in Parse.LetterOrDigit.Or(Parse.Chars("_")).Many().Text()
+                            //from space in Parse.WhiteSpace
+                            //from rparen in Parse.String("desc").Or(Parse.String("asc"))
+                            select content;*/
+            
+            CaseInsensitiveString = from space in Parse.WhiteSpace.Optional()
+                                    from lparen in Parse.Char('-').Or(Parse.Char('d')).Optional()
+                                    from content in Parse.LetterOrDigit.Or(Parse.Chars("_")).Many().Text()
+                                    select ScimExpression.String(content);
+
+            ExpressionInParentheses = from expr in CaseInsensitiveString
+                                      from space in Parse.WhiteSpace
+                                      from rparen in Parse.Ref(() => Filter)
+                                      select rparen;
+
+            Operand = (CaseInsensitiveString
+                ).Token();
 
             // compareOp = "-" / "d" valuePath "desc" /
             //        valuePath" / "" / "asc" /
-            Comparison = Parse.XChainOperator(
+            /*Comparison = Parse.XChainOperator(
                 Descending
                 .XOr(Ascending)
-                , ExpressionInParentheses.Token(), ScimExpression.Binary);
+                , Operand, ScimExpression.Binary);*/
 
+            Presence = Operand.SelectMany(operand => Descending.Or(Ascending), (operand, pr) => ScimExpression.Unary(pr, operand));
+
+            //AttributeExpression = Presence.Or(Comparison);
             // logExp    = FILTER SP (",") SP FILTER
-            Filter = Parse.XChainOperator(Delimiter, Comparison, ScimExpression.Binary);
+            //Filter = Parse.XChainOperator(Delimiter, Comparison, ScimExpression.Binary);
+            Filter = Parse.XChainOperator(Delimiter, Presence, ScimExpression.Binary);
         }
 
         public static ScimExpression ParseExpression(string expression)
---
+++ test/IdentityDirectory.Scim.Test/SortExpressionParserTests.cs
@@ -0,0 +1,18 @@
+﻿namespace IdentityDirectory.Scim.Test
+{
+    using System;
+    using IdentityDirectory.Scim.Query;
+    using Xunit;
+
+    public class SortExpressionParserTests
+    {
+        [Fact]
+        public void CanParseSimpleFilter()
+        {
+            var rootNode = SortExpressionParser.ParseExpression("Item1 asc, Item2 desc, Item3 asc, Item4 desc");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Delimiter(Delimiter(Delimiter(Ascending(Item1),Descending(Item2)),Ascending(Item3)),Descending(Item4))", rootNode.ToString());
+        }
+    }
+}
