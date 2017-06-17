841de308552bdc4b825526a59ce2b158d13f3db7
f7q <kokudou330@gmail.com>
2017-06-17 15:21:58 +0900

    Add Select Filter

---
+++ src/IdentityDirectory.Scim/Expressions/SelectExpressionParser.cs
@@ -0,0 +1,45 @@
+﻿namespace IdentityDirectory.Scim.Query
+{
+    using System;
+    using System.Linq;
+    using Sprache;
+
+    public class SelectExpressionParser
+    {
+        private static readonly Parser<string> Delimiter = Parse.Char(',').Token().Return("Delimiter");
+        
+        private static readonly Parser<ScimExpression> Filter;
+        private static readonly Parser<ScimExpression> ExpressionInParentheses;
+        private static readonly Parser<ScimExpression> CaseInsensitiveString;
+        private static readonly Parser<ScimExpression> Operand;
+        private static readonly Parser<ScimExpression> Presence;
+
+        /// <summary>
+        /// 仮想コンストラクタ
+        /// </summary>
+        static SelectExpressionParser()
+        {
+            CaseInsensitiveString = from space in Parse.WhiteSpace.Many().Optional()
+                                    from content in Parse.LetterOrDigit.Or(Parse.Chars("_")).Many().Text()
+                                    from space2 in Parse.WhiteSpace.Many().Optional()
+                                    select ScimExpression.String(content);
+            ExpressionInParentheses = from content in Parse.Ref(() => Filter)
+                                      select content;
+            Operand = (CaseInsensitiveString
+                ).Token();
+            
+            // logExp    = FILTER SP (",") SP FILTER
+            //Filter = ExpressionInParentheses.SelectMany(operand => Delimiter, (operand, pr) => ScimExpression.Unary(pr, operand));
+            Filter = Parse.XChainOperator(Delimiter, CaseInsensitiveString, ScimExpression.Binary);
+        }
+
+        public static ScimExpression ParseExpression(string expression)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+            return Filter.End().Parse(expression);
+        }
+    }
+}
---
+++ test/IdentityDirectory.Scim.Test/SelectExpressionParserTests.cs
@@ -0,0 +1,45 @@
+﻿namespace IdentityDirectory.Scim.Test
+{
+    using System;
+    using IdentityDirectory.Scim.Query;
+    using Xunit;
+
+    public class SelectExpressionParserTests
+    {
+        [Fact]
+        public void CanParseSimpleFilter()
+        {
+            var rootNode = SelectExpressionParser.ParseExpression("Item1, Item2, Item3, Item4");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Delimiter(Delimiter(Delimiter(Item1,Item2),Item3),Item4)", rootNode.ToString());
+        }
+
+        [Fact]
+        public void CanParseSimpleFilter2()
+        {
+            var rootNode = SelectExpressionParser.ParseExpression("   Item1   ,  Item2   ,   Item3     ");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Delimiter(Delimiter(Item1,Item2),Item3)", rootNode.ToString());
+        }
+
+        [Fact]
+        public void CanParseSimpleFilter3()
+        {
+            var rootNode = SelectExpressionParser.ParseExpression("Item2,Item3");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Delimiter(Item2,Item3)", rootNode.ToString());
+        }
+
+        [Fact]
+        public void CanParseSimpleFilter4()
+        {
+            var rootNode = SelectExpressionParser.ParseExpression("Item1");
+            Assert.NotNull(rootNode);
+            Console.WriteLine(rootNode);
+            Assert.Equal("Item1", rootNode.ToString());
+        }
+    }
+}
