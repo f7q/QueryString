2566b5485e95eb30f669658cd0fa00c151a02eb6
f7q <kokudou330@gmail.com>
2017-06-17 15:24:30 +0900

    Delete Comment

--- src/IdentityDirectory.Scim/Expressions/SelectExpressionParser.cs
+++ src/IdentityDirectory.Scim/Expressions/SelectExpressionParser.cs
@@ -9,10 +9,7 @@
         private static readonly Parser<string> Delimiter = Parse.Char(',').Token().Return("Delimiter");
         
         private static readonly Parser<ScimExpression> Filter;
-        private static readonly Parser<ScimExpression> ExpressionInParentheses;
         private static readonly Parser<ScimExpression> CaseInsensitiveString;
-        private static readonly Parser<ScimExpression> Operand;
-        private static readonly Parser<ScimExpression> Presence;
 
         /// <summary>
         /// 仮想コンストラクタ
@@ -23,13 +20,8 @@
                                     from content in Parse.LetterOrDigit.Or(Parse.Chars("_")).Many().Text()
                                     from space2 in Parse.WhiteSpace.Many().Optional()
                                     select ScimExpression.String(content);
-            ExpressionInParentheses = from content in Parse.Ref(() => Filter)
-                                      select content;
-            Operand = (CaseInsensitiveString
-                ).Token();
             
             // logExp    = FILTER SP (",") SP FILTER
-            //Filter = ExpressionInParentheses.SelectMany(operand => Delimiter, (operand, pr) => ScimExpression.Unary(pr, operand));
             Filter = Parse.XChainOperator(Delimiter, CaseInsensitiveString, ScimExpression.Binary);
         }
 
